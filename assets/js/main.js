6900d825f6ef9bb7aa81bca835bd711e7116fad1
David Yoder <david.yoder@live.com>
2023-09-12 22:44:04 -0400

    Added HTML count and cursor coordinates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
 /* javascript */
 
-var bauhaus = `
 
-Staaatliches Bauhaus (1919-1933)
+const mouseXElement = document.getElementById("mouseX");
+const mouseYElement = document.getElementById("mouseY");
 
-      ..-""T-..
-    ,'__   |   '.
-   /   N|  |     !
-  :     '  |      :
-  [      __|      ]
-  :      _I       :
-   !   ___I      /
-    '.  N]     ,'
-      "-"!..--"
+document.addEventListener("mousemove", (e) => {
+  mouseXElement.textContent = e.clientX;
+  mouseYElement.textContent = e.clientY;
+});
 
-`;
 
-console.log(bauhaus);
+const elementsInPortrait = document.querySelectorAll(".torso, .neck, .face, .back-hair-right, .back-hair-left, .top-hair-left, .top-hair-mid, .top-hair-right, .second-top-hair-left, .second-top-hair-mid, .second-top-hair-right, .nose, .mouth, .eye-white-left, .eye-iris-left, .eye-pupil-left, .eye-white-right, .eye-iris-right, .eye-pupil-right");
+const totalElements = elementsInPortrait.length;
+console.log(`Total elements in the portrait: ${totalElements}`);
